d25c1357fee5ff9fe749a02927fca7b76c73e7fe
Akbarzade <Akbarzade@me.com>
2016-07-28 23:46:53 +0430

    Initial commit for Air Fl8Deck

---
+++ Fl8D/Views/TerminalCharts/ChartTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChartTableViewCell.swift
+//  Fl8D
+//
+//  Created by Akbarzade on 7/28/16.
+//  Copyright © 2016 Akbarzade. All rights reserved.
+//
+
+import UIKit
+
+class ChartTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
